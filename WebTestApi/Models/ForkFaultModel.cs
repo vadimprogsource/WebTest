b0abcc00cb17d04925486031c0b91059da757e5f
Vadim <dementey@outlook.com>
2025-06-07 15:08:07 +0300

    add faul date patch

--- WebTestApi/Models/ForkFaultModel.cs
+++ WebTestApi/Models/ForkFaultModel.cs
@@ -9,16 +9,15 @@ namespace TestWebApi.Models
     
 
         public int Id { get; set; }
-        public DateTime ProblemDetectedAt { get; set; }
+        public DateTime? ProblemDetectedAt { get; set; }
         public DateTime? ProblemResolvedAt { get; set; }
         public string? Reason { get; set; }
 
         public TimeSpan Downtime { get;  }
 
-        DateTime IForkFault.ProblemDetectedAt => ProblemDetectedAt.ToUniversalTime();
-        DateTime? IForkFault.ProblemResolvedAt => ProblemResolvedAt.HasValue?ProblemResolvedAt.Value.ToUniversalTime(): ProblemDetectedAt.AddHours(1);
-
         string IForkFault.Reason => Reason ?? string.Empty;
+
+        DateTime IForkFault.ProblemDetectedAt => ProblemDetectedAt.HasValue ? ProblemDetectedAt.Value.ToUniversalTime() : DateTime.UtcNow;
 
         public ForkFaultModel() { }
 
@@ -26,7 +25,7 @@ namespace TestWebApi.Models
         {
             Id = source.Id;
             ProblemDetectedAt = source.ProblemDetectedAt.ToLocalTime();
-            ProblemResolvedAt = source.ProblemResolvedAt.HasValue?source.ProblemResolvedAt.Value.ToLocalTime(): ProblemDetectedAt.AddHours(1);
+            ProblemResolvedAt = source.ProblemResolvedAt.HasValue?source.ProblemResolvedAt.Value.ToLocalTime(): null;
             Reason = source.Reason;
             Downtime = source.Downtime;
         }
