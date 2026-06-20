a123dadf419f7ee5c23ec376b3f17f301ddb0c19
Vadim <dementey@outlook.com>
2026-06-20 19:11:05 +0300

    add service bus

---
+++ Test.Api/Infrastructure/IServiceBus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Api.Infrastructure;
+
+
+public interface IMessageReceiver<TMessage>
+{
+    void Receive(IServiceBusContext context, TMessage message);
+}
+
+
+public interface IServiceBusContext
+{
+    void Send<TMessage>(TMessage message);
+}
+
+public interface IServiceBus
+{
+    IServiceBusContext Context { get; }
+    void Register<TMessage>(IMessageReceiver<TMessage> receiver);
+}
